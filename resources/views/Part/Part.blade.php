ac4498cb8998724b0fa4a8bdaf5b42ef7f5e43de
Wama Software <wamasoftware14@gmail.com>
2015-02-16 11:42:36 +0530

    part file testing changes (to display records)

--- resources/views/Part/Part.blade.php
+++ resources/views/Part/Part.blade.php
@@ -1,7 +1,7 @@
 
 @extends('layouts.main')
 @section('content')
-<a href="{{ action('PartController@partList')}}">List</a> 
+ <a href="{{ action('PartController@partList')}}">List</a> 
 <a href="{{ action('PartController@addPart')}}">Add</a> 
 <a href="{{ action('PartController@editPart')}}">Edit</a> 
 <br/>
@@ -27,5 +27,74 @@
     </thead>
     <tbody>
     </tbody>
-</table>
+</table> 
+<?php
+/*
+<div class="wrapper">
+    <div class="row">
+        <div class="col-md-12">
+            <section class="panel">
+                <header class="panel-heading custom-tab dark-tab">
+                    <ul class="nav nav-tabs">
+                        <li class="active"><a href="#" >List</a></li>
+                        <li><a href="/part/add">Add</a></li>
+                    </ul>
+                </header>
+                <div class="panel-body">
+                    <div class="tab-content">
+                        <div class="tab-pane active" id="List">
+                            <form class="form-horizontal">                               
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <div class="panel panel-default">
+                                            <div class="panel-heading">
+                                                <h3 class="panel-title"><i class="fa fa-bars"></i> Part Number</h3>
+                                            </div>
+                                            <div class="panel-body">
+
+                                                <div class="table-responsive">
+                                                    <table  class="display table table-bordered table-striped" id="dynamic-table">
+                                                        <thead>
+                                                        <tr>
+                                                            <th>SKU</th>
+                                                            <th>Description</th>
+                                                            <th>Cost</th>
+                                                            <th>Currency</th>
+                                                            <th>Action</th>
+                                                        </tr> 
+                                                        </thead>
+                                                        <tbody>
+                                                       <?php 
+                                                        /*<tr class="gradeX">
+                                                            <td>BF0013</td>
+                                                            <td>Barcelona FC sport Jersey</td>
+                                                            <td>13</td>
+                                                            <td>USA</td>
+                                                            <td><a href="{{action('PartController@addPart')}}" class="btn btn-primary"><span class="fa fa-pencil"></span></a></td>
+                                                        </tr>
+                                                        <tr class="gradeX">
+                                                            <td>MA0011</td>
+                                                            <td>Barcelona FC sport Jersey</td>
+                                                            <td>17</td>
+                                                            <td>USA</td>
+                                                            <td><a href="{{action('PartController@addPart')}}" class="btn btn-primary"><span class="fa fa-pencil"></span></a></td>
+                                                        </tr>
+                                                         
+                                                        </tbody>
+                                                    </table>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+    </div>
+
+</div>
+ */ ?>
 @endsection
