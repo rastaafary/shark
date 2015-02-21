97b8a963fd548fd7a1b402449d970558768c498e
Wama Software <wamasoftware14@gmail.com>
2015-02-21 18:32:13 +0530

    Client Side Validation on PO Customer

--- public/js/PoCustomer.js
+++ public/js/PoCustomer.js
@@ -4,93 +4,9 @@ $(document).ready(function () {
         if ($('#addNew').is(':checked') ? $("#newdetails").show() : $("#newdetails").hide())
             ;
     });
-     jQuery.validator.addMethod("onlyname", function (value, element) {
+    jQuery.validator.addMethod("onlyname", function (value, element) {
         return this.optional(element) || /^[a-z A-Z]+$/.test(value);
     }, "Please enter valid name.");
-
-    $("#btnSubmit").click(function () {
-        if ($('#addNew').is(':checked')) {
-          //  alert("hii");
-            $('#PoCustomer').validate({
-                rules: {
-                    'comp_name': {
-                        required: true,
-                        onlyname: true
-                    },
-                    'zipcode': {
-                        required: true
-                    },
-                    'building_no': {
-                        required: true,
-                    },
-                    'street_addrs': {
-                        required: true
-                    },
-                    'phone_no': {
-                        required: true,
-                        mobileNo: true
-                    },
-                    'interior_no': {
-                        required: true
-                    },
-                    'city': {
-                        required: true
-                    },
-                    'state': {
-                        required: true
-                    },
-                    'identifer': {
-                        required: true
-                    }                   
-                },
-                messages: {
-                    'comp_name': {
-                        required: 'Please enter company name.',
-                        onlyname: 'Please enter valid company name.'
-                    },
-                    'building_no': {
-                        required: 'Please enter building no.'
-                    },
-                    'street_addrs': {
-                        required: 'Please enter street address.'
-                    },
-                    'phone_no': {
-                        required: 'Please enter phone no.',
-                        mobileNo: 'Please enter valid phone no.'
-                    },
-                    'interior_no': {
-                        required: 'Please enter interior no.'
-                    },
-                    'city': {
-                        required: 'Please enter city.'
-                    },
-                    'state': {
-                        required: 'Please enter state.'
-                    },                  
-                    'zipcode': {
-                        required: 'Please enter zipcode.'
-                    },
-                     'identifer': {
-                        required: 'Please enter identifer.'
-                    }
-                },
-                highlight: function (element) {
-                    $(element).removeClass("textinput");
-                    $(element).addClass("errorHighlight");
-                },
-                unhighlight: function (element) {
-                    $(element).removeClass("errorHighlight");
-                    $(element).addClass("textinput");
-                },
-                errorPlacement: function (error, element) {
-                    error.insertAfter(element);
-                }
-            });
-          //  alert("Hello");
-
-        }
-
-    });
 
     $("#POCustomer-list").dataTable({
         //"bProcessing": true,
@@ -105,10 +21,40 @@ $(document).ready(function () {
                 "data": aoData,
                 "success": fnCallback
             });
-        },
+        }
     });
+    
     $('#PoCustomer').validate({
         rules: {
+            'comp_name': {
+                required: true,
+                onlyname: true
+            },
+            'zipcode': {
+                required: true
+            },
+            'building_no': {
+                required: true
+            },
+            'street_addrs': {
+                required: true
+            },
+            'phone_no': {
+                required: true,
+                mobileNo: true
+            },
+            'interior_no': {
+                required: true
+            },
+            'city': {
+                required: true
+            },
+            'state': {
+                required: true
+            },
+            'identifer': {
+                required: true
+            },
             'shippingMethod': {
                 required: true
             },
@@ -123,9 +69,38 @@ $(document).ready(function () {
             },
             'Ai': {
                 required: true
-            },
+            }
         },
         messages: {
+            'comp_name': {
+                required: 'Please enter company name.',
+                onlyname: 'Please enter valid company name.'
+            },
+            'building_no': {
+                required: 'Please enter building no.'
+            },
+            'street_addrs': {
+                required: 'Please enter street address.'
+            },
+            'phone_no': {
+                required: 'Please enter phone no.',
+                mobileNo: 'Please enter valid phone no.'
+            },
+            'interior_no': {
+                required: 'Please enter interior no.'
+            },
+            'city': {
+                required: 'Please enter city.'
+            },
+            'state': {
+                required: 'Please enter state.'
+            },
+            'zipcode': {
+                required: 'Please enter zipcode.'
+            },
+            'identifer': {
+                required: 'Please enter identifer.'
+            },
             'shippingMethod': {
                 required: 'Please enter shipping method.'
             },
